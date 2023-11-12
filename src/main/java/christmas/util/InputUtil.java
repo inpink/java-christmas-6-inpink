10727093668ff2134850330060722030a5019193
inpink <inpink@kakao.com>
2023-11-13 01:23:03 +0900

    feat: Console API 이용하여 입력받기

---
+++ src/main/java/christmas/util/InputUtil.java
@@ -0,0 +1,10 @@
+package christmas.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+
+    public static String inputString() {
+        return Console.readLine();
+    }
+}
