54ea496cb72b8773d77a40eb3236648ba86d8e97
inpink <inpink@kakao.com>
2023-11-13 00:51:49 +0900

    feat: 정수가 아니면 예외 발생
    ErrorMessage를 받아와서 Util이 다른 곳에 의존하지 않고, 재사용성을 더높임

--- src/main/java/christmas/validation/IntegerValidator.java
+++ src/main/java/christmas/validation/IntegerValidator.java
@@ -1,15 +1,16 @@
 package christmas.validation;
 
-import static christmas.messages.ErrorMessages.INVALID_NEGATIVE_VALUE_MESSAGE;
-
 import christmas.util.ExceptionUtil;
 import christmas.util.IntegerUtil;
+import java.math.BigInteger;
 
 public class IntegerValidator {
 
-    public static void validateNotNegative(int value) {
-        if (IntegerUtil.isNegative(value)) {
-            ExceptionUtil.throwInvalidValueException(INVALID_NEGATIVE_VALUE_MESSAGE.getMessage());
+    public static void validateInteger(final String string, final String errorMessage) {
+        if (!IntegerUtil.isInteger(string)) {
+            ExceptionUtil.throwInvalidValueException(errorMessage);
         }
-    }
+    }  //
+
+
 }
