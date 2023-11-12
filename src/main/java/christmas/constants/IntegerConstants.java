a4b2bd25e6f12cbe9c20c1bb37829dcf5177b1e9
inpink <inpink@kakao.com>
2023-11-13 01:06:24 +0900

    feat: 프로젝트 전역에서 사용되는 int형 상수를 enum으로 관리

---
+++ src/main/java/christmas/constants/IntegerConstants.java
@@ -0,0 +1,21 @@
+package christmas.constants;
+
+public enum IntegerConstants {
+
+    THIS_MONTH(12),
+    MINIMUM_EVENT_APPLY_PRICE(1000),
+    MAX_MENU_COUNT(20),
+    MIN_VISIT_DATE_RANGE(1),
+    MAX_VISIT_DATE_RANGE(31);
+
+    private final int value;
+
+    IntegerConstants(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
