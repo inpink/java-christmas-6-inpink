0ff4c766220dae90ac71c5608ae639e31f1dd1bc
inpink <inpink@kakao.com>
2023-11-13 02:52:31 +0900

    test: 해당 Menu에 특정 Item이름이 있는지 상태 확인

--- src/test/java/christmas/domain/menu/MenuTest.java
+++ src/test/java/christmas/domain/menu/MenuTest.java
@@ -3,7 +3,6 @@ package christmas.domain.menu;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import christmas.util.ExceptionUtil;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,9 +15,9 @@ public class MenuTest {
             "초코케이크, DESSERTS",
             "제로콜라, DRINKS"
     })
-    public void 아이템이_어떤메뉴에_해당되는지_찾기(String itemName, Menu expectedMenuName) {
+    public void 아이템이_어떤메뉴에_해당되는지_찾기(final String itemName, final Menu expectedMenuName) {
         // When
-        Menu result = Menu.findMenuType(itemName);
+        final Menu result = Menu.findMenuType(itemName);
 
         // Then
         assertThat(result).isEqualTo(expectedMenuName);
@@ -26,29 +25,31 @@ public class MenuTest {
 
     @ParameterizedTest
     @CsvSource({
-            "없는음식, APPETIZERS",
-            "안팔아요, MAIN_DISHES",
-            "배고파요, DESSERTS",
-            "RED_WINE, DRINKS"
+            "없는음식",
+            "안팔아요",
+            "배고파요",
+            "RED_WINE",
+            "''"
     })
-    public void 메뉴판에_없는아이템을_찾으면_예외_발생(String itemName, Menu expectedMenuName) {
+    public void 메뉴판에_없는아이템을_찾으면_예외_발생(final String itemName) {
         // When && Then
         assertThatThrownBy(() -> Menu.findMenuType(itemName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("존재하지 않는 메뉴입니다.");
     }
 
-/*    @ParameterizedTest
+    @ParameterizedTest
     @CsvSource({
-            "DESSERTS, 초콜릿 케이크, true",
-            "DESSERTS, 마법의 물약, false",
-            "DRINKS, 레몬 에이드, true",
-            "DRINKS, 마법의 물약, false"
+            "DESSERTS, 초코케이크, true",
+            "DESSERTS, 도마뱀수프, false",
+            "DRINKS, 샴페인, true",
+            "DRINKS, 버터맥주, false"
     })
-    public void contains_테스트(String menuName, String itemName, boolean expected) {
-        Menu menu = Menu.valueOf(menuName);
-        boolean result = menu.contains(itemName);
+    public void contains_테스트(final Menu menu, final String itemName, final boolean expected) {
+        // When
+        final boolean result = menu.contains(itemName);
 
-        assertEquals(expected, result);
-    }*/
+        // Then
+        assertThat(result).isEqualTo(expected);
+    }
 }
