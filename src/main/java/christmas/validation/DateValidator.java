1416970f05e06d875e3c0393c431bfc31960fa2e
inpink <inpink@kakao.com>
2023-11-13 16:22:06 +0900

    feat: String 연,월, 일을 받아 달력에 있는 옳은 날짜인지 검증

---
+++ src/main/java/christmas/validation/DateValidator.java
@@ -0,0 +1,23 @@
+package christmas.validation;
+
+import christmas.util.ExceptionUtil;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class DateValidator {
+
+    public static void validateExistInCalendar(final String year,
+                                               final String month,
+                                               final String date,
+                                               final String errorMessage) {
+        IntegerValidator.validateInteger(errorMessage, year, month, date);
+
+        try {
+            LocalDate.of(Integer.parseInt(year),
+                    Integer.parseInt(month),
+                    Integer.parseInt(date));
+        } catch (DateTimeException e) {
+            ExceptionUtil.throwInvalidValueException(errorMessage);
+        }
+    }
+}
