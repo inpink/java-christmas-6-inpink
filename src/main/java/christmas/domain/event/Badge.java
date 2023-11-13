de50192024b49068fe71783e6e5a409be04f3ca6
inpink <inpink@kakao.com>
2023-11-14 02:03:39 +0900

    refactor: "없음" 뱃지 추가, int 원시형 대신 DiscountPrice Wrapper class 사용.
    Money의 공통 특징을 사용할 수 있고, 추후 Money의 조건이 변경되더라도 Badge의 코드 변화는 최소화

--- src/main/java/christmas/domain/event/Badge.java
+++ src/main/java/christmas/domain/event/Badge.java
@@ -1,32 +1,33 @@
 package christmas.domain.event;
 
+import static christmas.messages.ErrorMessages.INVALID_BADGE_PRICE;
+
+import christmas.domain.money.DiscountPrice;
+import christmas.util.ExceptionUtil;
 import java.util.Arrays;
 
 public enum Badge {
 
+    NO("없음", 0, 4_999),
     STAR("별", 5_000, 9_999),
     TREE("트리", 10_000, 19_999),
     SANTA("산타", 20_000, Integer.MAX_VALUE);
 
     private final String name;
-    private final int minBenefitsPrice;
-    private final int maxBenefitsPrice;
+    private final DiscountPrice minBenefitsPrice;
+    private final DiscountPrice maxBenefitsPrice;
 
     Badge(final String name, final int minBenefitsPrice, final int maxBenefitsPrice) {
         this.name = name;
-        this.minBenefitsPrice = minBenefitsPrice;
-        this.maxBenefitsPrice = maxBenefitsPrice;
+        this.minBenefitsPrice = new DiscountPrice(minBenefitsPrice);
+        this.maxBenefitsPrice = new DiscountPrice(maxBenefitsPrice);
     }
 
-    public static Badge getBadgeByPrice(final int price) {
+    public static Badge getBadgeByPrice(final DiscountPrice price) {
         return Arrays.stream(Badge.values())
-                .filter(badge -> badge.isPriceInRange(price))
+                .filter(badge -> price.isInRange(badge.minBenefitsPrice, badge.maxBenefitsPrice))
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(()
+                        -> ExceptionUtil.returnInvalidValueException(INVALID_BADGE_PRICE.getMessage()));
     }
-
-    private boolean isPriceInRange(final int price) {
-        return price >= minBenefitsPrice && price <= maxBenefitsPrice;
-    }
-
 }
