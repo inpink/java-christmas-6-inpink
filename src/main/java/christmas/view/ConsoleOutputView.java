07b96197c73cf8f76e58f7865c52cfbd3dcc340c
inpink <inpink@kakao.com>
2023-11-13 14:30:07 +0900

    feat: 결과 출력 화면의 모든 title 출력

--- src/main/java/christmas/view/ConsoleOutputView.java
+++ src/main/java/christmas/view/ConsoleOutputView.java
@@ -1,7 +1,75 @@
 package christmas.view;
 
+
+import static christmas.messages.OutputMessages.BENEFITS_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.GIFT_MENU_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.ORDER_MENU_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.PREVIEW_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.THIS_MONTH_EVENT_BADGE_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.TOTAL_BENEFITS_PRICE_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.TOTAL_PRICE_AFTER_DISCOUNT_TITLE_MESSAGE;
+import static christmas.messages.OutputMessages.TOTAL_PRICE_BEFORE_DISCOUNT_TITLE_MESSAGE;
+
+import christmas.util.OutputUtil;
+
 public class ConsoleOutputView implements OutputView {
 
+    @Override
+    public void outputPreviewTitle() {
+        System.out.println(makeTitleForm(PREVIEW_TITLE_MESSAGE.getMessage()));
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputOrderMenu() {
+        System.out.println(makeTitleForm(ORDER_MENU_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputTotalPriceBeforeDiscount() {
+        System.out.println(makeTitleForm(TOTAL_PRICE_BEFORE_DISCOUNT_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputGiftMenu() {
+        System.out.println(makeTitleForm(GIFT_MENU_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputBenefits() {
+        System.out.println(makeTitleForm(BENEFITS_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputTotalBenefitsPrice() {
+        System.out.println(makeTitleForm(TOTAL_BENEFITS_PRICE_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputTotalPriceAfterDiscount() {
+        System.out.println(makeTitleForm(TOTAL_PRICE_AFTER_DISCOUNT_TITLE_MESSAGE.getMessage()));
+
+        OutputUtil.printEmptyLine();
+    }
+
+    @Override
+    public void outputThisMonthBadge() {
+        System.out.println(makeTitleForm(THIS_MONTH_EVENT_BADGE_TITLE_MESSAGE.getMessage()));
 
 
+    }
+
+    private String makeTitleForm(String title) {
+        return "<" + title + ">";
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,5 +2,19 @@ package christmas.view;
 
 public interface OutputView {
 
+    void outputPreviewTitle();
 
+    void outputOrderMenu();
+
+    void outputTotalPriceBeforeDiscount();
+
+    void outputGiftMenu();
+
+    void outputBenefits();
+
+    void outputTotalBenefitsPrice();
+
+    void outputTotalPriceAfterDiscount();
+
+    void outputThisMonthBadge();
 }
