92c9993260c3d25dffac5f75c22917ece32e7092
inpink <inpink@kakao.com>
2023-11-15 03:07:47 +0900

    feat: 기능들을 엮어 전체 흐름 구성

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,9 +1,24 @@
 package christmas.controller;
 
+import static christmas.constants.IntegerConstants.THIS_MONTH;
+import static christmas.constants.IntegerConstants.THIS_YEAR;
+
+import christmas.domain.dto.DtoMapper;
+import christmas.domain.dto.MemberBadgeDto;
+import christmas.domain.dto.OrderBenefitsDto;
+import christmas.domain.dto.OrderItemsDto;
+import christmas.domain.entity.Money;
+import christmas.domain.event.Badge;
+import christmas.domain.event.Benefits;
+import christmas.domain.event.Event;
+import christmas.domain.menu.Items;
+import christmas.domain.order.Order;
 import christmas.service.OrderService;
+import christmas.validation.DateValidator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import christmas.view.StartView;
+import java.time.LocalDate;
 
 public class OrderController {
 
@@ -26,16 +41,43 @@ public class OrderController {
     public void order() {
         printStartMessages();
 
-        final String dateOfVisit = inputView.inputExpectedDateOfVisit();
-        final String itemsWithCount = inputView.inputOrderItemsWithCounts();
+        final String inputDateOfVisit = inputView.inputExpectedDateOfVisit();
+        DateValidator.validateExistInCalendar(
+                THIS_YEAR.toString(),
+                THIS_MONTH.toString(),
+                inputDateOfVisit);
+
+        final LocalDate dateOfVisit = LocalDate.of(
+                THIS_YEAR.getValue(),
+                THIS_MONTH.getValue(),
+                Integer.parseInt(inputDateOfVisit));
+
+        final String inputItemsWithCounts = inputView.inputOrderItemsWithCounts();
+        final Items items = Items.create(inputItemsWithCounts);
+        final Money totalPrice = items.calcTotalPrice();
+
+        final Benefits benefits = Event.findBenefits(dateOfVisit, items, totalPrice);
+        final Money trickeryDiscount = benefits.calcTrickeryDiscount();
+        final Order order = Order.create(dateOfVisit, items, benefits);
+        final Badge badge = Badge.getBadgeByPrice(benefits.calcTotalDiscount());
 
         outputView.outputPreviewTitle();
-        outputView.outputOrderMenu();
-        outputView.outputTotalPriceBeforeDiscount();
-        outputView.outputGifts();
-        outputView.outputDiscounts();
-        outputView.outputTotalPriceAfterDiscount();
-        outputView.outputThisMonthBadge();
+
+        final OrderItemsDto orderItemsDto = DtoMapper.toOrderItemsDto(
+                items.toMapWithNameKey(),
+                totalPrice.getAmount());
+        outputView.outputOrderItems(orderItemsDto);
+
+        final OrderBenefitsDto orderBenefitsDto = DtoMapper.toOrderBenefitsDto(
+                benefits.toGiftsMapWithNameKey(),
+                benefits.toDiscountsMapWithNameKey(),
+                totalPrice.getAmount(),
+                trickeryDiscount.getAmount());
+
+        outputView.outputBenefits(orderBenefitsDto);
+
+        final MemberBadgeDto memberBadgeDto = DtoMapper.toMemberBadgeDto(badge.name());
+        outputView.outputThisMonthBadge(memberBadgeDto);
     }
 
     private void printStartMessages() {
