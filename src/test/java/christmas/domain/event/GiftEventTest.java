fc5e6781ad31300ce01fff13ed794a7c5f3081dc
inpink <inpink@kakao.com>
2023-11-14 21:43:41 +0900

    test: 혜택의 discount 가격까지 검증

--- src/test/java/christmas/domain/event/GiftEventTest.java
+++ src/test/java/christmas/domain/event/GiftEventTest.java
@@ -2,7 +2,9 @@ package christmas.domain.event;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDate;
 import christmas.domain.entity.Money;
 
@@ -10,21 +12,27 @@ class GiftEventTest {
 
     @ParameterizedTest
     @CsvSource({
-            "2023-12-10, 12000, 1", // 이벤트 기간 내, 최소 주문 금액 충족
-            "2023-12-26, 12000, 0", // 이벤트 기간 이후, 최소 주문 금액 충족
-            "2023-12-10, 11999, 0", // 이벤트 기간 내, 최소 주문 금액 미충족
-            "2023-11-30, 12000, 0"  // 이벤트 기간 이전, 최소 주문 금액 충족
+            "2023-12-10, 120000, 1, 25000", // 이벤트 기간 내, 최소 주문 금액 충족
+            "2023-12-26, 120000, 1, 25000", // 이벤트 기간 내, 최소 주문 금액 충족
+            "2023-12-10, 119999, 0, 0", // 이벤트 기간 내, 최소 주문 금액 미충족
+            "2024-01-26, 120000, 0, 0", // 이벤트 기간 이후, 최소 주문 금액 충족
+            "2023-11-30, 120000, 0, 0"  // 이벤트 기간 이전, 최소 주문 금액 충족
     })
-    void 증정_이벤트_계산(final String date, final int totalOrderPrice, final int expectedResult) {
+    public void 증정_이벤트_계산(final String date,
+                          final int totalOrderPrice,
+                          final int expectedGiftsSize,
+                          final int expectedDiscountPrice) {
         // Given
         final LocalDate testDate = LocalDate.parse(date);
         final Money orderPrice = Money.create(totalOrderPrice);
 
         // When
-        Benefit benefit = GiftEvent.calculateBenefit(testDate, orderPrice);
-        Gifts gifts = benefit.getGifts();
+        final Benefit benefit = GiftEvent.calculateBenefit(testDate, orderPrice);
+        final Gifts gifts = benefit.getGifts();
+        final Money money = benefit.getDiscountPrice();
 
         // Then
-        assertThat(gifts.size()).isEqualTo(expectedResult);
+        assertThat(gifts.size()).isEqualTo(expectedGiftsSize);
+        assertThat(money.getAmount()).isEqualTo(expectedDiscountPrice);
     }
 }
